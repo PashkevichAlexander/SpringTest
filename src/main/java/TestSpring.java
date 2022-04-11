b9baf48d22ca221a0aac1917aa9126d1f43daf5c
Alexander Pashkevich <pashkevich.a.a@mail.ru>
2022-04-11 20:33:23 +0300

    Refactor and cleanup Code

--- src/main/java/TestSpring.java
+++ src/main/java/TestSpring.java
@@ -2,12 +2,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext contex = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer musicPlayer = contex.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
-        contex.close();
+        context.close();
     }
 }

ce6a28bb36e6dd2b55ec05b593c65d67064e4a1d
Alexander Pashkevich <pashkevich.a.a@mail.ru>
2022-04-18 23:32:05 +0300

    Test Scope, make destroy and init method

--- src/main/java/ClassicalMusic.java
+++ src/main/java/ClassicalMusic.java
@@ -1,4 +1,13 @@
 public class ClassicalMusic implements Music{
+    private ClassicalMusic(){ }
+
+
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+
+
+
     @Override
     public String getSong() {
         return "pianoMusic";
--- src/main/java/TestSpring.java
+++ src/main/java/TestSpring.java
@@ -4,6 +4,15 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        RockMusic rockMusic = context.getBean("musicBean1", RockMusic.class);
+        System.out.println(rockMusic.getSong());
+
+        ClassicalMusic classicalMusic = context.getBean("musicBean2", ClassicalMusic.class);
+        ClassicalMusic classicalMusic1 = context.getBean("musicBean2", ClassicalMusic.class);
+        System.out.println(classicalMusic.equals(classicalMusic1));
+
+
+
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
         System.out.println(musicPlayer.getName());
